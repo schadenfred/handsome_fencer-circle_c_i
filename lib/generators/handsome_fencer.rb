926df354db97eece1bdf10cf7e33578182ccdfe3
schadenfred <fred.schoeneman@gmail.com>
2018-09-26 16:28:16 -0700

    require expose generator

--- lib/generators/handsome_fencer.rb
+++ lib/generators/handsome_fencer.rb
@@ -1,3 +1,4 @@
 require_relative 'handsome_fencer/circle_c_i/install_generator.rb'
 require_relative 'handsome_fencer/circle_c_i/deploy_key_generator.rb'
 require_relative 'handsome_fencer/circle_c_i/obfuscated_env_files_generator.rb'
+require_relative 'handsome_fencer/circle_c_i/exposed_env_files_generator.rb'
