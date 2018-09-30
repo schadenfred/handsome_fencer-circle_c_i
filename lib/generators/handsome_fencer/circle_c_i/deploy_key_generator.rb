f7ed445a8a6525b661d5ab83abfa8b8ff3f5a0f3
schadenfred <fred.schoeneman@gmail.com>
2018-09-29 21:10:04 -0700

    added new keys

--- lib/generators/handsome_fencer/circle_c_i/deploy_key_generator.rb
+++ lib/generators/handsome_fencer/circle_c_i/deploy_key_generator.rb
@@ -6,13 +6,26 @@ module HandsomeFencer
       source_root File.expand_path('templates', __dir__)
       desc "generate deploy key"
 
+      class_option :environment, type: :string, default: 'deploy'
       def generate_deploy_key
+        environment = options[:environment]
         @cipher = OpenSSL::Cipher.new 'AES-128-CBC'
         @salt = '8 octets'
         @new_key = @cipher.random_key
 
-        create_file ".circleci/deploy.key", Base64.encode64(@new_key)
+        create_file ".circleci/#{environment}.key", Base64.encode64(@new_key)
       end
     end
   end
 end
+
+# desc 'copy keys to server'
+# task :environment_keys, [:source, :destination] do |task, args|
+#
+#   on server do
+#     within deploy_path do
+#       destination = args[:destination] || '.'
+#       upload! File.expand_path("../../#{args[:source]}", __dir__), destination='.'
+#     end
+#   end
+# end
--- lib/generators/handsome_fencer/circle_c_i/obfuscated_env_files_generator.rb
+++ lib/generators/handsome_fencer/circle_c_i/obfuscated_env_files_generator.rb
@@ -3,11 +3,17 @@ module HandsomeFencer
   module CircleCI
 
     class ObfuscatedEnvFilesGenerator < Rails::Generators::Base
+
+      class_option :environment, type: :string, default: 'deploy'
+
       source_root File.expand_path('templates', __dir__)
       desc "obfuscate .env files inside .circleci directory"
 
       def obfuscate_env_files
-        @cipher = HandsomeFencer::CircleCI::Crypto.new
+
+        environment = options[:environment]
+
+        @cipher = HandsomeFencer::CircleCI::Crypto.new(dkfile: environment)
         @cipher.obfuscate
       end
     end
