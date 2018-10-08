fe0aa1255186e64b9c452a3760d13915e34f1a19
schadenfred <fred.schoeneman@gmail.com>
2018-10-08 15:40:29 -0700

    updated

--- lib/handsome_fencer/circle_c_i/cli.rb
+++ lib/handsome_fencer/circle_c_i/cli.rb
@@ -1,30 +1,31 @@
 require 'thor'
 require 'handsome_fencer/circle_c_i/cli/hn'
 
+module HandsomeFencer
+  module CircleCI
+    class HammerOfTheGods < Thor
+      desc "hello NAME", "This will greet you"
+      long_desc <<-HELLO_WORLD
 
-module Socialinvestigator
-  class HammerOfTheGods < Thor
-    desc "hello NAME", "This will greet you"
-    long_desc <<-HELLO_WORLD
+      `hello NAME` will print out a message to the person of your choosing.
 
-    `hello NAME` will print out a message to the person of your choosing.
+      Brian Kernighan actually wrote the first "Hello, World!" program
+      as part of the documentation for the BCPL programming language
+      developed by Martin Richards. BCPL was used while C was being
+      developed at Bell Labs a few years before the publication of
+      Kernighan and Ritchie's C book in 1972.
 
-    Brian Kernighan actually wrote the first "Hello, World!" program
-    as part of the documentation for the BCPL programming language
-    developed by Martin Richards. BCPL was used while C was being
-    developed at Bell Labs a few years before the publication of
-    Kernighan and Ritchie's C book in 1972.
+      http://stackoverflow.com/a/12785204
+      HELLO_WORLD
+      option :upcase
+      def hello( name )
+        greeting = "Hello, #{name}"
+        greeting.upcase! if options[:upcase]
+        puts greeting
+      end
 
-    http://stackoverflow.com/a/12785204
-    HELLO_WORLD
-    option :upcase
-    def hello( name )
-      greeting = "Hello, #{name}"
-      greeting.upcase! if options[:upcase]
-      puts greeting
+      desc "hn COMMANDS", "Hacker News Control Module"
+      subcommand "hn", HandsomeFencer::CircleCI::CLI::Hn
     end
-
-    desc "hn COMMANDS", "Hacker News Control Module"
-    subcommand "hn", Socialinvestigator::CLI::Hn
   end
 end
